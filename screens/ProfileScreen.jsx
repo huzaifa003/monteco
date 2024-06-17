0ae5036bc5af48a90c44fd473a174211420fb32e
huzaifa003 <buntyhuzaifa3@gmail.com>
2024-06-17 18:27:29 +0500

    Added Modal

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
-import { View, Image, StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
-import { Button, Surface } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, SafeAreaView, Text, TouchableOpacity, Modal } from 'react-native';
+import { Button, Surface, Provider as PaperProvider } from 'react-native-paper';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import SegmentedControl from '../components/SegmentedControl';
+
 
 const ProfileScreen = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-                <View style={{ flex: 1, alignItems: 'flex-end', paddingTop: 10 }}>
-                    <TouchableOpacity onPress={() => console.log('Edit Pressed')}>
-                        <MaterialCommunityIcons name="pencil" size={24} color="black" />
-                    </TouchableOpacity>
+        
+            <SafeAreaView style={styles.container}>
+                
+                <View style={styles.header}>
+                    <View style={{ flex: 1, alignItems: 'flex-end', paddingTop: 30 }}>
+                        <TouchableOpacity onPress={() => setModalVisible(true)}>
+                            <MaterialCommunityIcons name="pencil" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
 
-            <View style={{ paddingHorizontal: 10, flex: 0.4, marginTop: 40 }}>
-                <Surface style={styles.surface}>
-                    <Image source={require('../assets/Icons/logo.png')} style={{ width: '100%', height: '100%', borderRadius: 40 }} />
-                </Surface>
-            </View>
+                <View style={{ paddingHorizontal: 10, flex: 0.4, marginTop: 40 }}>
+                    <Surface style={styles.surface}>
+                        <Image source={require('../assets/Icons/logo.png')} style={{ width: '100%', height: '100%', borderRadius: 40 }} />
+                    </Surface>
+                </View>
 
-            <Text style={styles.textStyle}>Name: Shumail</Text>
-            <Text style={styles.textStyle}>Phone: +436601683385</Text>
-            <Text style={styles.textStyle}>Region: Leinz</Text>
-            <Text style={styles.textStyle}>Language: en</Text>
-            
-            <Button
-                mode="contained"
-                style={{ marginTop: 20, width: '100%', backgroundColor: '#fc3503', borderRadius: 10, paddingVertical: 5}}
-                onPress={() => console.log('Pressed')}>
-                Logout
-            </Button>
-        </SafeAreaView>
+                <Text style={styles.textStyle}>Name: Shumail</Text>
+                <Text style={styles.textStyle}>Phone: +436601683385</Text>
+                <Text style={styles.textStyle}>Region: Leinz</Text>
+                <Text style={styles.textStyle}>Language: en</Text>
+
+                <Button
+                    mode="contained"
+                    style={{ marginTop: 20, width: '100%', backgroundColor: '#fc3503', borderRadius: 10, paddingVertical: 5 }}
+                    onPress={() => console.log('Pressed')}>
+                    Logout
+                </Button>
+
+                <Modal style={{height:'20%'}}
+                    animationType='fade'
+                    transparent={true}
+                    
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.modalView}>
+                        <SegmentedControl options={['English', 'German', 'Italian']} />
+                        <Text></Text>
+                        <Button onPress={() => setModalVisible(!modalVisible)}>Close</Button>
+                    </View>
+                </Modal>
+            </SafeAreaView>
+        
     );
 }
 
@@ -58,10 +81,27 @@ const styles = StyleSheet.create({
         fontFamily: 'Poppins-Black',
         fontWeight: 'bold',
         marginTop: 20,
-        marginRight: 25
+        marginRight: 25,
     },
     header: {
         flexDirection: 'row',
         width: '100%',
+    },
+    modalView: {
+        flex: 0.1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
 });
